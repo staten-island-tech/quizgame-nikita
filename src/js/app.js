ec5bed1b71eacc924310b91387124ef4c8cf3f97
nikitaerastov <71673216+nikitaerastov@users.noreply.github.com>
2021-04-05 18:44:19 -0400

    did template for one question
    wrote a function for later that will notice the correct answer

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
-console.log("connected");
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+}
+
+const questions = [
+  {
+    questionContent: "knorfjeroghogrejg",
+    choices: [
+      { choice: "gntgkk", correct: false },
+      { choice: "jrbjbrog", correct: true },
+      { choice: "jnforfrofjbb", correct: false },
+      { choice: "jbrjfjrfjr", correct: false },
+    ],
+  },
+];
